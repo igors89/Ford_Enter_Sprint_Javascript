aad6df053aac06551bfe432ea33c19145458d958
IGOR ANDRADE SOARES <igsoares89@gmail.com>
2026-05-15 11:30:24 -0300

    commit automatico

--- js/carousel.js
+++ js/carousel.js
@@ -9,6 +9,11 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
+    constructor (nome, text, link){
+        this.nome=nome;
+        this.text=text;
+        this.link=link;
+    }
     
       
     static Start(arr){
@@ -18,7 +23,7 @@ class Carousel {
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
             }
             
         } else {
@@ -27,6 +32,18 @@ class Carousel {
     }
 
     static Next(){
+
+        const container = document.getElementById("carousel");
+        const titulo = document.getElementById("carousel-title");
+
+        if(Carousel._sequence >= Carousel._size) Carousel._sequence=0;
+
+        let item = carouselArr[Carousel._sequence];
         
+        container.innerHTML = `<img src="img/${item.nome}" alt="${item.nome}"  />`
+        titulo.innerHTML = `<a>${item.text}</a>`
+        
+        Carousel._sequence++;
+
     }
 };
--- js/form.js
+++ js/form.js
@@ -2,26 +2,41 @@
 //class contato
 
 class contato {
-    
+    constructor(nome,sobrenome,email,cpf,telefone,contato){
+        this.nome=nome;
+        this.sobrenome=sobrenome;
+        this.email=email;
+        this.cpf=cpf;
+        this.telefone=telefone;
+        this.contato=contato;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
+    let data = new contato(form.elements.namedItem("nome").value,
             form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-  
+    Enviar(data);
+
 }
 
-function Enviar() {
+function Enviar(valor) {
 
-    var nome = document.getElementById("nomeid");
+    document.addEventListener("submit", (event) => {
+        event.preventDefault();
+        var nome = document.getElementById("nomeid");
+    
+        if (nome.value != "") {
+            alert('Obrigado sr(a) ' + valor.nome + ' os seus dados foram encaminhados com sucesso');
+        }
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+        console.log(valor);
+
+        // document.getElementById('meuform').submit();
+    })
 
 }
